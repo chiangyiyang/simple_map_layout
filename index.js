e3f16e0c96ab0151eaeb79ed9d6ceaedf1e6e1d9
Yiyang <chiangyiyang@gmail.com>
2021-05-17 20:45:14 +0800

    3. 增加一個對話框

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const add_point = ()=> console.log('Add a point...')
+// Add point to map
+const add_point = ()=> {
+  // Hide the input dialog
+  document.getElementById('point_dialog').style.display='none';
+
+  // Get parameters of point
+  const lon = document.getElementById('pt_lon').value;
+  const lat = document.getElementById('pt_lat').value;
+  
+  console.log(`Add a new point at (${lon}, ${lat})`);
+  
+  // Add a new point
+  
+  // Fly to the point
+}
+
+
+// Open a dialog for input parameters for new point
+const show_point_dialog = ()=> {
+  document.getElementById('point_dialog').style.display='block';
+}
