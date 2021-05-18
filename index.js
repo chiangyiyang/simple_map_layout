25ddaea09d9ec909fd5b9762eec73955e3a6aec6
Yiyang <chiangyiyang@gmail.com>
2021-05-18 13:54:47 +0800

    6. 增加右鍵功能(有Bug)

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const add_point = () => {
   // Add a new point
 
   var entity = viewer.entities.add({
-    position: Cesium.Cartesian3.fromDegrees(lon, lat),
+    position: Cesium.Cartesian3.fromDegrees(lon, lat, 0),
     ellipse: {
       semiMinorAxis: 1000.0,
       semiMajorAxis: 1000.0,
@@ -49,3 +49,48 @@ var viewer = new Cesium.Viewer('cesiumContainer', {
   fullscreenButton: false,
 });
 viewer._cesiumWidget._creditContainer.style.display = 'none';
+
+viewer.entities.add({
+  id: 'cur_pos',
+  label: {
+    // position : Cesium.Cartesian2.ZERO, 
+    show: true   // Removed semicolon here
+  }
+});
+
+viewer.scene.canvas.addEventListener('mousemove', function (e) {
+  var entity = viewer.entities.getById('cur_pos');
+  var ellipsoid = viewer.scene.globe.ellipsoid;
+  // Mouse over the globe to see the cartographic position 
+  var cartesian = viewer.camera.pickEllipsoid(new Cesium.Cartesian3(e.clientX, e.clientY), ellipsoid);
+  if (cartesian) {
+    var cartographic = ellipsoid.cartesianToCartographic(cartesian);
+    var longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(10);
+    var latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(10);
+    entity.position = cartesian;
+    entity.label.show = true;
+    entity.label.font_style = 84;
+    //entity.position= Cesium.Cartesian2.ZERO; 
+    entity.label.text = '(' + longitudeString + ', ' + latitudeString + ')';
+    var result = entity.label.text;  // We can reuse this
+    document.getElementById("mouse_info").innerHTML = result;
+  } else {
+    entity.label.show = false;
+  }
+});
+
+
+viewer.scene.canvas.addEventListener('contextmenu', function (e) {
+  var ellipsoid = viewer.scene.globe.ellipsoid;
+  var cartesian = viewer.camera.pickEllipsoid(new Cesium.Cartesian3(e.clientX, e.clientY), ellipsoid);
+  if (cartesian) {
+    var cartographic = ellipsoid.cartesianToCartographic(cartesian);
+    var longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(10);
+    var latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(10);
+    console.log('(' + longitudeString + ', ' + latitudeString + ')');
+    // Set parameters of point
+    document.getElementById('pt_lon').value=longitudeString;
+    document.getElementById('pt_lat').value=latitudeString;
+    show_point_dialog();
+  }
+});
